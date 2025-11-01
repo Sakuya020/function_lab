10e9524f69a5fb0a3f9baf28cb3a28e53f34a7a4
Sakuya020 <42985269+Sakuya020@users.noreply.github.com>
2025-11-01 09:57:23 +0900

    revalidate every 24 hours

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -34,8 +34,8 @@ export const metadata = {
   },
 };
 
-// revalidate every 1 hour
-export const revalidate = 3600;
+// revalidate every 24 hours
+export const revalidate = 24 * 60 * 60;
 
 export default function RootLayout({ children }) {
   return (
